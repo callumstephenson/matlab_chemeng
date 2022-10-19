ca7465942562f88d286cf0432b18530ee7570253
callumstephenson <36637180+callumstephenson@users.noreply.github.com>
2022-10-19 10:43:19 +0100

    added axis labels

--- michaelmas_computing/task4/runscript.m
+++ michaelmas_computing/task4/runscript.m
@@ -5,5 +5,7 @@ x = linspace(376.4,397.3,50);
 for i = 1:length(x)
    list(i) = fzero(@(vf) rachford_rice(x(i), 0.4, vf), 0.5);
 end
-list
-plot(x, list)
+
+plot(x, list);
+xlabel("Temperature of flash vessel / K");
+ylabel("V/F mole fraction");
