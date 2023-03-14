a5c276581802bfff1bd488397c881179a51b7288
callumstephenson <callumstephenson03@gmail.com>
2023-03-14 00:05:31 +0000

    finished

--- lab_data_processing/friction_smooth_pipes/water_runscript.m
+++ lab_data_processing/friction_smooth_pipes/water_runscript.m
@@ -1,34 +1,34 @@
 clear
 % exp data
-m_1 = [19, 25, 30, 35, 46, 55, 63, 75, 84, 99, 117, 128, 142, 159, 180,...
-    193, 230, 258, 280, 305, 311, 342, 348, 369, 418] * 0.001;
-m_2 = [14, 16, 19, 20, 21, 23, 25, 28, 32, 50, 66, 70, 80, 83,...
-    94, 100, 120, 129, 137, 148, 155, 161, 171, 183, 203] * 0.001;
+m_1 = [6, 8, 14, 20, 28, 40, 53, 59, 70, 85, 90, 95, 110, 119, 135, 158,...
+    175, 204, 215, 232, 238] * 0.001;
+m_2 = [11, 13, 17, 19, 22, 26, 29, 32, 35, 52, 64, 64, 75, 83, 92, 107,...
+    117, 133, 141, 152, 156] * 0.001;
 
 % phys data
-rho_air = 1.20;
-mu_air = 1.82e-5;
+rho_water = 998;
+mu_water = 1e-3;
 d_pipe = 0.007;
-d_orifice = 0.004;
+d_orifice = 0.0041;
 beta = d_orifice / d_pipe;
 L = 1.5;
 
 for i = 1:length(m_1)
 
     % vel calc
-    u(i) = 0.7 * beta.^2 * sqrt((2 * m_1(i) * sind(20) * 9.81 * 760)/ (rho_air * (1-beta.^2)));
+    u(i) = 0.7 * beta.^2 * sqrt((2 * m_1(i) * 9.81 * rho_water)/ (rho_water * (1-beta.^2)));
     u_err(i) = u(i) * (0.0005/m_1(i));
 
     % re calc
-    re(i) = rho_air * u(i) * d_pipe / mu_air;
+    re(i) = rho_water * u(i) * d_pipe / mu_water;
     re_err(i) = re(i) * (0.0005/m_1(i));
 
     % cf calc
-    cf(i) = (m_2(i) * sind(20) * 9.81 * 0.007 * 760) / (rho_air * 2 * L * u(i).^2);
+    cf(i) = (m_2(i) * 9.81 * 0.007 * rho_water) / (rho_water * 2 * L * u(i).^2);
     cf_err(i) = cf(i) * sqrt((0.001/m_2(i)).^2 + (u_err(i)/u(i)).^2);
 
-    
-    if re <= 2600
+        
+    if re <= 2500
         cf_theoretical(i) = 16/re(i);
     else
         cf_theoretical(i) = 0.079 * re(i).^(-0.25);
